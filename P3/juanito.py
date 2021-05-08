47f2bfca0ff367665dcb0f7cefb3ea2772eb96f9
juanvs00 <55741983+juanvs00@users.noreply.github.com>
2021-05-08 13:10:38 +0200

    Add files via upload

--- P3/juanito.py
+++ P3/juanito.py
@@ -39,33 +39,45 @@ def sacarRestricciones(registro):
 
 def evaluarNodos(grafo, registro):
 
-    nodos1=[]
-    for i in range(len(grafo[0])):
-        if(grafo[0][i] != 0):
-            nodos1.append(grafo[0][i])
+    nodos1=grafo[0][1:]
+    #for i in range(1,len(grafo[0])):
+    #    if(grafo[0][i] != 0):
+    #        nodos1.append(grafo[0][i])
+            
     nodos2=registro[::2]
+    cont=0
     for i in range(len(nodos1)):
         for j in range(len(nodos2)):
             if(nodos1[i] == nodos2[j]):
-                nodos2[j]=0
+                #nodos2[j]=0
+                nodos2.pop(j)
+                cont+=1
                 break
-    for i in range(len(nodos2)):
-        if(nodos2[i] != 0):
-            return False
-    return True
+    #for i in range(len(nodos2)):
+    #    if(nodos2[i] != 0):
+    #        return False
+    #return True
+    #print(cont)
+    if cont==len(nodos1):
+        return True
+    else:
+        return False
 
 def evaluarRestricciones(grafo, registro):
 
     #Consigue los nodos del grafo y el registro
-    nodos1=[]
-    for i in range(len(grafo[0])):
-        if(grafo[0][i] != 0):
-            nodos1.append(grafo[0][i])
+    #nodos1=[]
+    #for i in range(1,len(grafo[0])):
+    #    if(grafo[0][i] != 0):
+    #        nodos1.append(grafo[0][i])
+
+    nodos1=grafo[0][1:]
     nodos2=registro[::2]
 
     #
     restricciones=sacarRestricciones(registro)
     #
+    acierto=True
     for i in range(1,len(grafo)):
         for j in range(1,len(grafo[i])):
             if(i != j):
@@ -76,11 +88,13 @@ def evaluarRestricciones(grafo, registro):
                         if(k != l):
                             if(restricciones[0][k]==ind1 and restricciones[l][0]==ind2):
                                 found=False
-                                for m in grafo[i][j]:
+                                for m in list(range(grafo[i][j][0],grafo[i][j][1]+1)):
                                     if(restricciones[k][l] == m):
                                         found=True
                                 if(found==False):
                                     return False
+    return acierto
+
 
 def crearGrafo(registro):
     
@@ -121,63 +135,47 @@ def adaptarGrafo(grafo, registro):
                                 if(restricciones[0][k]==ind1 and restricciones[l][0]==ind2):
                                     for m in grafo[i][j]:
                                         if(restricciones[k][l] != m):
-                                            if(restricciones[k][l]>0 and grafo[i][j][1]<restricciones[k][l]):
-                                                grafo[i][j]=[grafo[i][j][0], restricciones[k][l]]
+                                            if(grafo[i][j][1]<restricciones[k][l]):
+                                                grafo[i][j][1]= restricciones[k][l]
                                             else:
-                                                if(restricciones[k][l]<0 and grafo[i][j][0]>restricciones[k][l]):
-                                                    grafo[i][j]=[restricciones[k][l],grafo[i][j][len(grafo[i][j])-1]]
+                                                if(grafo[i][j][0]>restricciones[k][l]):
+                                                    grafo[i][j][0]=restricciones[k][l]
     
                                                 
     return grafo
 
 def evaluarGrafo(grafo, datos):
-
+    aciertos=0
+    evN=0
     for i in range(len(datos)):
         if(evaluarNodos(grafo, datos[i])):
+            evN+=1
             if(evaluarRestricciones(grafo, datos[i])):
-                print("Hola")
+                aciertos+=1
+
+    return evN,aciertos
             
 
 def main():
 
     datos=leerDatos()
-    
-    grafo=crearGrafo(datos[34])
 
-    evaluarGrafo(grafo, datos)
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-    #Conseguir nodos del grafo mejorado
-    #nodos1=[]
-    #for i in range(len(grafo[0])):
-    #    if(grafo[0][i] != 0):
-    #        nodos1.append(grafo[0][i])
-    #nodos2=registro[::2]
-    #nodosFinal=nodos1
-    #
-    #aux=nodos2
-    #for i in range(len(nodos1)):
-    #    for j in range(len(aux)):
-    #        if(nodos1[i] == aux[j]):
-    #            aux[j]=0
-    #            break
-    #for i in range(len(aux)):
-    #    if(aux[i] != 0):
-    #        nodosFinal.append(aux[i])
+    i=0
+    while i<20:
+        aux=random.randint(0,999)
+        evN=0
+        if(len(datos[aux])>=8):
+            grafo=crearGrafo(datos[aux])
+            for j in range(len(datos)):
+                grafo=adaptarGrafo(grafo,datos[j])
+            for k in range(len(datos)):
+                if(evaluarNodos(grafo, datos[k])):
+                    evN+=1
+            print("\nTanda ",i,": ",evN)
+            i=i+1
+            print("GRAFO: ")
+            for l in range(len(grafo)):
+                print(grafo[l])
 
 
 if __name__ == "__main__":
