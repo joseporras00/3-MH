bc14c8c4e2dcf819add43e07acb6b4b0307ede78
joseporras00 <55441394+joseporras00@users.noreply.github.com>
2021-05-08 13:46:43 +0200

    Add files via upload

--- P3/pruebav3.py
+++ P3/pruebav3.py
@@ -11,27 +11,6 @@ def leerDatos():
             datos.append(re.split(":| ",linea))
     return datos
 
-def unicoRegistro(registro1, datos):
-    unico=True
-    nodos1=registro1[::2]
-    while unico==True:
-        for m in range(len(datos)):
-            nodos2=datos[m][::2]
-            if len(nodos1)==len(nodos2):
-                cont=0
-                aux=nodos2
-                for i in range(len(nodos1)):
-                    for j in range(i+1,len(nodos2)):
-                        if(nodos1[i] == nodos2[j]):
-                            nodos2.pop(j)
-                            nodos1.pop(i)
-                            break
-
-                if len(nodos1)!=cont:
-                    unico=False
-
-    return unico
-
 
 def sacarRestricciones(registro):
 
@@ -74,32 +53,6 @@ def evaluarNodos(grafo, registro):
         return False
 
 
-def evaluarRestricciones(grafo, registro):
-
-    #Consigue los nodos del grafo y el registro
-    nodos1=grafo[0][1:]
-    nodos2=registro[::2]
-
-    #
-    restricciones=sacarRestricciones(registro)
-    #
-    acierto=True
-    for i in range(1,len(grafo)):
-        for j in range(1,len(grafo[i])):
-            if(i != j):
-                ind1 = grafo[0][i]
-                ind2 = grafo[j][0]
-                for k in range(1,len(restricciones)):
-                    for l in range(1,len(restricciones[k])):
-                        if(k != l):
-                            if(restricciones[0][k]==ind1 and restricciones[l][0]==ind2):
-                                found=False
-                                for m in list(range(grafo[i][j][0],grafo[i][j][1]+1)):
-                                    if(restricciones[k][l] == m):
-                                        found=True
-                                if(found==False):
-                                    return False
-    return acierto
 
 
 def crearGrafo(registro):
@@ -148,25 +101,12 @@ def adaptarGrafo(grafo, registro):
     return grafo
 
     
-
-def evaluarGrafo(grafo, datos):
-    aciertos=0
-    for i in range(len(datos)):
-        if(evaluarNodos(grafo, datos[i])):
-            if(evaluarRestricciones(grafo, datos[i])):
-                aciertos+=1
-
-    return aciertos
     
 
-def hillClimbing(registro,datos):
-    #l=len(datos)
-    ##Creamos una solucion aleatoria
-    #registros = list(range(l))
-    #registro = datos[random.randint(0, len(registros) - 1)]
+def iniciar(registro,datos):
     grafo = crearGrafo(registro)
 
-    ##Mejoramos el grafo
+    ##Mejoramos el grafo y sacamos respectivos aciertos
     aciertos=0
     for i in range(len(datos)):
         if(evaluarNodos(grafo, datos[i])):
@@ -174,25 +114,23 @@ def hillClimbing(registro,datos):
             aciertos+=1
         
 
-    #aciertos=evaluarGrafo(grafo,datos)
-
     return grafo, aciertos
 
 
 def ils(inicial, datos):
 
-    iteraciones = 20
+    i = 20
     grafoSol = inicial[0]
     maxAciertos = inicial[1]
 
-    while iteraciones > 0:
-
-        solucion = hillClimbing(datos)
-        if solucion[1] > maxAciertos :
-            grafo = solucion[0]
-            aciertos = solucion[1]
-        iteraciones = iteraciones - 1
-    return grafo, aciertos
+    while i > 0:
+        aux=random.randint(0,len(datos))
+        solucion = iniciar(datos[i],datos)
+        #if solucion[1] > maxAciertos :
+        #    grafo = solucion[0]
+        #    aciertos = solucion[1]
+        print("\nTanda ",i,": ",solucion[1])
+        i = i - 1
     
 def main():
 
@@ -200,26 +138,23 @@ def main():
 
     resultados=[]
     datos2=[]
-    datos3=[]
     print(len(datos))
 
     for i in range(len(datos)):
         if len(datos[i])>=8:
             datos2.append(datos[i])
 
-    print(len(datos2))
+    inicial=iniciar(datos2[0],datos2)
+    ils(inicial,datos2)
     
-    for i in range(100):
-        print(i,"\n")
-        sol=hillClimbing(datos2[i],datos2)
 
 
-        resultados.append([i,sol[0], sol[1]])
-        i+=1
-    with open("Grafos.csv", "w") as file:
-        file.write(",".join(["N", "Grafo","Aciertos"]))
-        for res in resultados:
-                   file.write(",".join(str(e) for e in res)+"\n")
+    #resultados.append([i,sol[0], sol[1]])
+    
+    #with open("Grafos.csv", "w") as file:
+    #    file.write(",".join(["N", "Grafo","Aciertos"]))
+    #    for res in resultados:
+    #               file.write(",".join(str(e) for e in res)+"\n")
 
 
 
